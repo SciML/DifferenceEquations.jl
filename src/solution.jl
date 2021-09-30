7a83538c8bb3f876edca4cf01dc52f7b6374f3aa
Cameron Pfiffer <cpfiffer@gmail.com>
2021-09-30 11:40:50 -0700

    Add docstring, ignore Manifest.toml

--- src/solution.jl
+++ src/solution.jl
@@ -1,3 +1,11 @@
+"""
+    StateSpaceSolution{T1, T2, T3, T4, T5}
+
+Wrapper function containing the observables `z`, 
+simulated hidden state `u`, evolution shocks `W`, 
+prior variances `P`, and the `likelihood` if
+it is available.
+"""
 struct StateSpaceSolution{T1, T2, T3, T4, T5}
     z::T1 # observables, if relevant
     u::T2 # hidden state, or mean of prior if filtering/estimating
@@ -5,4 +13,3 @@ struct StateSpaceSolution{T1, T2, T3, T4, T5}
     P::T4 # prior variances if filtering, would prefer actually having priors instead
     likelihood::T5  #likelihood of observables
 end
-
