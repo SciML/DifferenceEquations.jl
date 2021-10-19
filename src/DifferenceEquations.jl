0b90d8ad1f7b0932a583a096d6757ccd4ac81348
cpfiffer <cameron@pfiffer.org>
2021-10-19 10:33:47 -0700

    Imports/exports/promote_noise

--- src/DifferenceEquations.jl
+++ src/DifferenceEquations.jl
@@ -3,18 +3,31 @@ module DifferenceEquations
 using SciMLBase
 using Distributions
 using LinearAlgebra
+using CommonSolve
 
 import SciMLBase: SciMLProblem
 
 abstract type DifferenceProblem <: SciMLProblem end
 abstract type AbstractStateSpaceProblem{isinplace} <: DifferenceProblem end
 
+# Yuck hate this so much
+promote_noise(x, y) = [x], [y]
+promote_noise(x, y::AbstractArray) = [x], y
+promote_noise(x::AbstractArray, y) = x, [y]
+promote_noise(x::AbstractArray, y::AbstractArray) = x, y
+
+include("noise.jl")
 include("alg.jl")
-include("problem.jl")
+include("linear.jl")
+include("nonlinear.jl")
 include("solution.jl")
 
 # Exports
 export StateSpaceProblem,
-    ConditionalGaussian
+    ConditionalGaussian,
+    LinearGaussian,
+    LinearStateSpaceProblem,
+    StandardGaussian,
+    DefinedNoise
 
 end # module
