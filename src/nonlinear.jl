58f34859567bd0238ab2c36e11542ba4a5830339
Peifan Wu <wupeifan@gmail.com>
2021-12-24 06:20:26 -0800

    Update nonlinear.jl

--- src/nonlinear.jl
+++ src/nonlinear.jl
@@ -28,7 +28,7 @@ function StateSpaceProblem(
     u0::utype,
     tspan::ttype,
     params = nothing;
-    obs_noise = MvNormal(diagm(ones(size(h(u0, params, 0))))), # Assume the default measurement error is MvNormal with identity covariance
+    obs_noise =  (h0 = h(u0, params, t0); MvNormal(Zeros{eltype(h0)}(length(h0)), I)), # Assume the default measurement error is MvNormal with identity covariance
     observables = nothing,
     noise = StandardGaussian(size(u0)),
 ) where {
