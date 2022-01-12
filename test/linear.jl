9f68c67dee045f00f88af5290802002c975439fb
Peifan Wu <wupeifan@gmail.com>
2022-01-12 02:14:50 -0800

    add kalman test

--- test/linear.jl
+++ test/linear.jl
@@ -1,5 +1,4 @@
 using ChainRulesTestUtils, DifferenceEquations, Distributions, DistributionsAD, LinearAlgebra, Test, Zygote
-using FiniteDiff: finite_difference_gradient
 
 # Matrices from RBC
 A = [0.9568351489231076 6.209371005755285; 3.0153731819288737e-18 0.20000000000000007]
@@ -15,6 +14,7 @@ observables = [[0.06065740756093304, 1.8761799441716314], [-1.9307042321264407, 
 # noise = [randn(1) for _ in 1:T]
 noise = [[0.25588177046623284], [0.0414605537784824], [-0.4828412176701489], [0.9852386982044963], [1.2118950851729577]]
 
+# joint case
 function joint_likelihood_1(A, B, C, u0, noise, observables, D)
     problem = LinearStateSpaceProblem(A, B, C, u0, (0, length(noise)); obs_noise = TuringDiagMvNormal(zeros(length(D)), D), noise, observables)
     return solve(problem, NoiseConditionalFilter(); save_everystep = false).loglikelihood
@@ -23,15 +23,10 @@ end
 @testset "linear rbc joint likelihood" begin
     @test joint_likelihood_1(A, B, C, u0, noise, observables, D) ≈ -536.0372569648741
     @inferred joint_likelihood_1(A, B, C, u0, noise, observables, D) # would this catch inference problems in the solve?
-    # NOTE: inference fails for the next line, hence the test fails
     test_rrule(Zygote.ZygoteRuleConfig(), joint_likelihood_1, A, B, C, u0, noise, observables, D; rrule_f = rrule_via_ad, check_inferred = false)
 
     res = gradient(joint_likelihood_1, A, B, C, u0, noise, observables, D)
     @test res ≈ ([-2.4360645393856517 0.4516572166327964; -19.52943478490269 0.07594715659996228], [219.3752613486913; -286.4024053583998], [1.2115987849879737 0.08821398833652092; 2.68844038727624 -2.5440332554602327], [142.27544836238908, 872.6057912350849], [[2.5344570451477524], [0.05891403400153479], [-9.415566573103447], [-2.2282218173643584], [-0.11373358884039465]], [[-6.23838085127432, -187.61799441716312], [192.85571471832162, 12.954561911192375], [-55.09671860305764, 148.092971334607], [-10.749097877737764, -104.57369352549641], [-16.857238200340635, -32.73567460636627]], [4016.7602080912934, 6880.714243164064])
-    # @test finite_difference_gradient(A -> joint_likelihood_1(A, B, C, u0, noise, observables, D), A) ≈ res[1] rtol = 1e-5
-    # @test finite_difference_gradient(B -> joint_likelihood_1(A, B, C, u0, noise, observables, D), B) ≈ res[2] rtol = 1e-5
-    # @test finite_difference_gradient(C -> joint_likelihood_1(A, B, C, u0, noise, observables, D), C) ≈ res[3] rtol = 1e-5
-    # @test finite_difference_gradient(D -> joint_likelihood_1(A, B, C, u0, noise, observables, D), D) ≈ res[7] rtol = 1e-5
 
     # Redundant struct on those matrices
     x = (; A, B, C, u0, noise, observables, D)
@@ -45,3 +40,15 @@ end
 # noise = [randn(1) for _ in 1:T]
 # @time joint_likelihood_1(A, B, C, u0, noise, observables, D)
 # @time gradient(joint_likelihood_1, A, B, C, u0, noise, observables, D)
+
+# Kalman only
+function kalman_likelihood(A, B, C, u0, observables, D)
+    problem = LinearStateSpaceProblem(A, B, C, TuringDenseMvNormal(zeros(length(u0)), cholesky(diagm(ones(length(u0))))), (0, length(observables)); noise = nothing, obs_noise = TuringDiagMvNormal(zeros(length(D)), D), observables)
+    return solve(problem, KalmanFilter(); save_everystep = false).loglikelihood
+end
+
+@testset "linear rbc kalman likelihood" begin
+    @test kalman_likelihood(A, B, C, u0, observables, D) ≈ -377.89835856536564
+    @inferred kalman_likelihood(A, B, C, u0, observables, D) # would this catch inference problems in the solve?
+    test_rrule(Zygote.ZygoteRuleConfig(), kalman_likelihood, A, B, C, u0, observables, D; rrule_f = rrule_via_ad, check_inferred = false)
+end
