1fa302b5c045400bb1e2087593a161988ffee238
Peifan Wu <wupeifan@gmail.com>
2022-01-12 16:57:56 -0800

    add FVGQ and bump versions

--- test/linear.jl
+++ test/linear.jl
@@ -1,4 +1,6 @@
 using ChainRulesTestUtils, DifferenceEquations, Distributions, DistributionsAD, LinearAlgebra, Test, Zygote
+using CSV, DataFrames
+using FiniteDiff: finite_difference_gradient
 
 # Matrices from RBC
 A = [0.9568351489231076 6.209371005755285; 3.0153731819288737e-18 0.20000000000000007]
@@ -52,3 +54,31 @@ end
     @inferred kalman_likelihood(A, B, C, u0, observables, D) # would this catch inference problems in the solve?
     test_rrule(Zygote.ZygoteRuleConfig(), kalman_likelihood, A, B, C, u0, observables, D; rrule_f = rrule_via_ad, check_inferred = false)
 end
+
+# Load FVGQ data for checks
+path = joinpath(pkgdir(DifferenceEquations), "test", "data")
+file_prefix = "FVGQ20"
+A = Matrix(DataFrame(CSV.File(joinpath(path, "$(file_prefix)_A.csv"); header = false)))
+B = Matrix(DataFrame(CSV.File(joinpath(path, "$(file_prefix)_B.csv"); header = false)))
+C = Matrix(DataFrame(CSV.File(joinpath(path, "$(file_prefix)_C.csv"); header = false)))
+# D_raw = Matrix(DataFrame(CSV.File(joinpath(path, "$(file_prefix)_D.csv"); header = false)))
+D = ones(6) * 1e-3
+observables_raw = Matrix(DataFrame(CSV.File(joinpath(path, "$(file_prefix)_observables.csv"); header = false)))
+noise_raw = Matrix(DataFrame(CSV.File(joinpath(path, "$(file_prefix)_noise.csv"); header = false)))
+observables = [observables_raw[i, :] for i in 1:size(observables_raw, 1)]
+noise = [noise_raw[i, :] for i in 1:size(noise_raw, 1)]
+u0 = zeros(size(A, 1))
+
+@testset "linear FVGQ joint likelihood" begin
+    # The likelihood number is huge, so we just test the gradients.
+    # Note: test_rrule struggles on D. Hence we only test a subset of the inputs with FiniteDiff
+    res = gradient(joint_likelihood_1, A, B, C, u0, noise, observables, D)
+    @test finite_difference_gradient(A -> joint_likelihood_1(A, B, C, u0, noise, observables, D), A) ≈ res[1] rtol = 1e-3
+    @test finite_difference_gradient(B -> joint_likelihood_1(A, B, C, u0, noise, observables, D), B) ≈ res[2] rtol = 1e-3
+    @test finite_difference_gradient(C -> joint_likelihood_1(A, B, C, u0, noise, observables, D), C) ≈ res[3] rtol = 1e-3
+    @test finite_difference_gradient(u0 -> joint_likelihood_1(A, B, C, u0, noise, observables, D), u0) ≈ res[4] rtol = 1e-3
+    noise_grad = finite_difference_gradient(noise_mat -> joint_likelihood_1(A, B, C, u0, [noise_mat[i, :] for i in 1:size(noise_mat, 1)], observables, D), noise_raw)
+    @test [noise_grad[i, :] for i in 1:size(noise_raw, 1)] ≈ res[5] rtol = 1e-5
+end
+
+@testset ""
