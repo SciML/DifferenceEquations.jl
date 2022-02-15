906ea336d5f84a1bbc8d36ef7812c754b17d29a5
Jesse Perla <jesseperla@gmail.com>
2022-02-14 17:01:17 -0800

    In progress on the kalman updates

--- src/kalman.jl
+++ src/kalman.jl
@@ -1,55 +1,64 @@
-function _solve!(
-    prob::LinearStateSpaceProblem{isinplace, Atype, Btype, Ctype, wtype, Rtype, utype, ttype, otype},
-    solver::KalmanFilter,
-    args...;
-    kwargs...
-) where {isinplace, Atype, Btype, Ctype, wtype, Rtype<:Distribution, utype, ttype, otype}
+
+function _solve!(prob::LinearStateSpaceProblem{isinplace,Atype,Btype,Ctype,wtype,Rtype,utype,ttype,
+                                               otype}, solver::KalmanFilter, args...;
+                 kwargs...) where {isinplace,Atype,Btype,Ctype,wtype,Rtype<:Distribution,utype,
+                                   ttype,otype}
     # Preallocate values
     T = prob.tspan[2] - prob.tspan[1] + 1
-    A, B, C = prob.A, prob.B, prob.C
+    @unpack A, B, C, u0 = prob
+    N = length(u0)
+
     # The following line could be cov(prob.obs_noise) if the measurement error distribution is not MvNormal
     R = prob.obs_noise.Σ # Extract covariance from noise distribution
     B_prod = B * B'
 
+    # TODO: move to internal algorithm cache
+    # This method of preallocation won't work with staticarrays.  Note that we can't use eltype(mean(u0)) since it may be special case of zeros
+    u = [Vector{eltype(u0)}(undef, N) for _ in 1:T] # Mean of Kalman filter inferred latent states
+    P = [Matrix{eltype(u0)}(undef, N, N) for _ in 1:T] # Posterior variance of Kalman filter inferred latent states
+    z = [Vector{eltype(prob.observables)}(undef, size(prob.observables, 1)) for _ in 1:T] # Mean of observables, generated from mean of latent states
+    innovation = [Vector{eltype(prob.observables)}(undef, size(prob.observables, 1)) for _ in 1:T]
+    K = [Matrix{eltype(u0)}(undef, N, N) for _ in 1:T] # Gain
+    V = [Matrix{eltype(u0)}(undef, N, N) for _ in 1:T] # V
+    CP = [Matrix{eltype(u0)}(undef, N, N) for _ in 1:T] # C * P[t]
+    V_chol = [Cholesky{eltype(u0),Matrix{eltype(u0)}}(Matrix{eltype(u0)}(undef, N, N), 'U', 0)
+              for _ in 1:T]
+    #Cholesky{Float64,Matrix{Float64}}(Matrix{Float64}(undef, 2,2), 'U', 0)
+    # cholesky!(C_val, A, Val(false); check = false)
+
     # Gaussian Prior
-    # u0 has to be a multivariate Normal distribution
-    u0_mean = prob.u0.μ
-    u0_variance = prob.u0.Σ
-
-    u = Vector{Vector{eltype(u0_mean)}}(undef, T) # Mean of Kalman filter inferred latent states
-    P = Vector{Matrix{eltype(u0_mean)}}(undef, T) # Posterior variance of Kalman filter inferred latent states
-    z = Vector{Vector{eltype(u0_mean)}}(undef, T) # Mean of observables, generated from mean of latent states
-
-    u[1] = u0_mean
-    P[1] = u0_variance
-    z[1] = C * u[1]
+    u[1] .= mean(u0)
+    P[1] .= cov(u0)
+    z[1] .= C * u[1]
 
     loglik = 0.0
 
-    for t in 2:T
-        t_n = t - 1 + prob.tspan[1]
+    @inbounds for t in 2:T
         # Kalman iteration
-        u[t] = A * u[t - 1]
-        P[t] = A * P[t - 1] * A' + B_prod
-        z[t] = C * u[t]
+        u[t] .= A * u[t - 1]
+        P[t] .= A * P[t - 1] * A' + B_prod
+        z[t] .= C * u[t]
 
-        CP_t = C * P[t]
-        V = Symmetric(CP_t * C' + R)
-        loglik += logpdf(MvNormal(z[t], V), prob.observables[:, t_n])
-        K = CP_t' / V  # Kalman gain
-        u[t] += K * (prob.observables[:, t_n] - z[t])
-        P[t] -= K * CP_t
+        CP[t] .= C * P[t]
+        V[t] .= CP[t] * C' + R
+        V[t] .= (V[t] + V[t]') / 2 # classic hack to deal with stability of not being quite symmetric
+        V_chol[t] = cholesky(V[t], Val(false); check = false)
+        innovation[t] .= prob.observables[:, t - 1] - z[t]
+        loglik += logpdf(MvNormal(PDMat(V_chol[t])), innovation[t])
+        K[t] .= CP[t]' / V_chol[t]  # Kalman gain
+        u[t] += K[t] * innovation[t]
+        P[t] -= K[t] * CP[t]
     end
 
     return StateSpaceSolution(nothing, nothing, nothing, nothing, loglik)
 end
 
 function ChainRulesCore.rrule(::typeof(_solve!),
-    prob::LinearStateSpaceProblem{isinplace, Atype, Btype, Ctype, wtype, Rtype, utype, ttype, otype},
-    ::KalmanFilter,
-    args...;
-    kwargs...
-) where {isinplace, Atype, Btype, Ctype, wtype, Rtype, utype, ttype, otype}
+                              prob::LinearStateSpaceProblem{isinplace,Atype,Btype,Ctype,wtype,Rtype,
+                                                            utype,ttype,otype}, ::KalmanFilter,
+                              args...;
+                              kwargs...) where {isinplace,Atype,Btype,Ctype,wtype,Rtype,utype,ttype,
+                                                otype}
     # Preallocate values
     T = prob.tspan[2] - prob.tspan[1] + 1
     A, B, C = prob.A, prob.B, prob.C
@@ -67,6 +76,7 @@ function ChainRulesCore.rrule(::typeof(_solve!),
     P = Vector{Matrix{eltype(u0_mean)}}(undef, T) # Posterior variance of Kalman filter inferred latent states
     P_mid = Vector{Matrix{eltype(u0_mean)}}(undef, T)
     z = Vector{Vector{eltype(u0_mean)}}(undef, T) # Mean of observables, generated from mean of latent states
+    innovation = Vector{Vector{eltype(u0_mean)}}(undef, T) # temporary for innovations
 
     u[1] = u0_mean
     P[1] = u0_variance
@@ -93,7 +103,8 @@ function ChainRulesCore.rrule(::typeof(_solve!),
     function solve_pb(Δsol)
         Δlogpdf = Δsol.loglikelihood
         if iszero(Δlogpdf)
-            return (NoTangent(), Tangent{typeof(prob)}(), NoTangent(), map(_ -> NoTangent(), args)...)
+            return (NoTangent(), Tangent{typeof(prob)}(), NoTangent(),
+                    map(_ -> NoTangent(), args)...)
         end
         # A bunch of initializations here
         ΔP = zero(u0_variance)
@@ -120,7 +131,13 @@ function ChainRulesCore.rrule(::typeof(_solve!),
             ΔC += ΔCP_t * P_mid[t]'
             ΔP_mid += C' * ΔCP_t
             Δz += Δlogpdf * inv_V * (prob.observables[:, t_n] - z[t]) # Σ^-1 * (z_obs - z)
-            ΔV -= Δlogpdf * 0.5 * (inv_V - inv_V * (prob.observables[:, t_n] - z[t]) * (prob.observables[:, t_n] - z[t])' * inv_V) # -0.5 * (Σ^-1 - Σ^-1(z_obs - z)(z_obx - z)'Σ^-1)
+            ΔV -= Δlogpdf *
+                  0.5 *
+                  (inv_V -
+                   inv_V *
+                   (prob.observables[:, t_n] - z[t]) *
+                   (prob.observables[:, t_n] - z[t])' *
+                   inv_V) # -0.5 * (Σ^-1 - Σ^-1(z_obs - z)(z_obx - z)'Σ^-1)
             ΔC += ΔV * C * P_mid[t]' + ΔV' * C * P_mid[t]
             ΔP_mid += C' * ΔV * C
             ΔC += Δz * u_mid[t]'
@@ -132,7 +149,10 @@ function ChainRulesCore.rrule(::typeof(_solve!),
             Δu = A' * Δu_mid
         end
         ΔΣ = Tangent{typeof(prob.u0.Σ)}(; mat = ΔP)
-        return (NoTangent(), Tangent{typeof(prob)}(; A = ΔA, B = ΔB, C = ΔC, u0 = Tangent{typeof(prob.u0)}(; μ = Δu, Σ = ΔΣ)), NoTangent(), map(_ -> NoTangent(), args)...)
+        return (NoTangent(),
+                Tangent{typeof(prob)}(; A = ΔA, B = ΔB, C = ΔC,
+                                      u0 = Tangent{typeof(prob.u0)}(; μ = Δu, Σ = ΔΣ)), NoTangent(),
+                map(_ -> NoTangent(), args)...)
     end
     return sol, solve_pb
 end
