58ef2285dbd2c8128fcd988182c764c104a19d1d
Jesse Perla <jesseperla@gmail.com>
2022-03-12 12:55:05 -0800

    Fix BLAS vendor deprecation

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,13 +1,19 @@
 using DifferenceEquations, BenchmarkTools
 using Test, LinearAlgebra, Random
 
-# Change the number of threads if openblas.
-if (BLAS.vendor() == :openblas64)
+# Check if MKL or not
+julia_mkl = @static if VERSION < v"1.7"
+    LinearAlgebra.BLAS.vendor() === :mkl
+else
+    any(contains("mkl"), getfield.(LinearAlgebra.BLAS.get_config().loaded_libs, :libname))
+end
+
+if !julia_mkl
     openblas_threads = min(4, Int64(round(Sys.CPU_THREADS / 2)))
     BLAS.set_num_threads(openblas_threads)
 end
 
-println("Running Testsuite with Threads.nthreads() = $(Threads.nthreads()) BLAS.vendor = $(BLAS.vendor()), and BLAS.num_threads = $(BLAS.get_num_threads()) \n")
+println("Running Testsuite with Threads.nthreads = $(Threads.nthreads()) MKL = $julia_mkl, and BLAS.num_threads = $(BLAS.get_num_threads()) \n")
 
 # Benchmark groups
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 10.0 # 10 seconds per benchmark by default.
