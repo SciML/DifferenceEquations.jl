e6b176d22bea501cadeebc72fb6121fe8c7b5330
Jesse Perla <jesseperla@gmail.com>
2022-06-17 13:28:16 -0700

    Add observation noise if provided (#69)

--- test/linear_simulations.jl
+++ test/linear_simulations.jl
@@ -29,7 +29,7 @@ observables_rbc = readdlm(joinpath(pkgdir(DifferenceEquations), "test/data/RBC_o
     # todo: add in regression tests
 end
 
-@testset "basic inference, simulated noise, no observations" begin
+@testset "basic inference, simulated noise, no observations, no observation noise" begin
     T = 20
     prob = LinearStateSpaceProblem(A_rbc, B_rbc, u0_rbc, (0, T); C = C_rbc, syms = [:a, :b])
     @inferred LinearStateSpaceProblem(A_rbc, B_rbc, u0_rbc, (0, T); C = C_rbc, syms = [:a, :b])
@@ -38,4 +38,28 @@ end
     @inferred solve(prob)
 
     # todo: add in regression tests
+end
+
+@testset "basic inference, no simulated noise, no observations with observation noise" begin
+    T = 20
+    B_no_noise = zeros(2, 2)
+    u0 = [1.0, 0.5]
+    prob_no_noise = LinearStateSpaceProblem(A_rbc, B_no_noise, u0, (0, T); C = C_rbc,
+                                            syms = [:a, :b])
+
+    sol_no_noise = solve(prob_no_noise)
+
+    prob_obs_noise = LinearStateSpaceProblem(A_rbc, B_no_noise, u0, (0, T); C = C_rbc,
+                                             syms = [:a, :b], observables_noise = D_rbc)
+    @inferred LinearStateSpaceProblem(A_rbc, B_no_noise, u0, (0, T); C = C_rbc,
+                                      syms = [:a, :b], observables_noise = D_rbc)
+    sol_obs_noise = solve(prob_obs_noise)
+    @inferred solve(prob_obs_noise)
+
+    # check that if the variance of the noise is tiny it is identical
+    sol_tiny_obs_noise = solve(LinearStateSpaceProblem(A_rbc, B_no_noise, u0, (0, T); C = C_rbc,
+                                                       syms = [:a, :b],
+                                                       observables_noise = [1e-16, 1e-16]))
+    @test maximum(maximum.(sol_tiny_obs_noise.z - sol_no_noise.z)) < 1e-7  # still some noise 
+    @test maximum(maximum.(sol_tiny_obs_noise.z - sol_no_noise.z)) > 0.0  # but not zero
 end
