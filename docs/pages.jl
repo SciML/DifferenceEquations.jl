66d18162ac10ad3a4fa2f64be9f7d9edf1c872bf
Jesse Perla <jesseperla@gmail.com>
2022-08-04 13:27:03 -0700

    sciml format compliance

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,10 +1,9 @@
 pages = [
-        "DifferenceEquations.jl: Discrete-Time State Space Solution Methods" => "index.md",
-
-        "Examples" => [
-            "examples/state_space_types.md",
-            "examples/linear_state_space_examples.md",
-            "examples/quadratic_state_space_examples.md",
-            "examples/general_state_space_examples.md",
-        ],
-    ]
+    "DifferenceEquations.jl: Discrete-Time State Space Solution Methods" => "index.md",
+    "Examples" => [
+        "examples/state_space_types.md",
+        "examples/linear_state_space_examples.md",
+        "examples/quadratic_state_space_examples.md",
+        "examples/general_state_space_examples.md",
+    ],
+]
